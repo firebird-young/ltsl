913d328ccc4a052523176d293488a14af3ac83de
firebird-young <yanggoku12@gmail.com>
2026-09-15 17:19:01 +0800

    m

--- 分销/asset/admin-sidebar.js
+++ 分销/asset/admin-sidebar.js
@@ -22,6 +22,7 @@
                 { name: '分销中心',     href: '分销.html' },
                 { name: '分销记账',     href: '分销记账.html' },
                 { name: '分销提现',     href: '分销提现.html' },
+                { name: '课程详情',     href: '课程详情.html' },
             ]
         },
         {
